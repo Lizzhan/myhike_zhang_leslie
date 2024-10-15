776f928f8e860b1daa3e9a50b716a9b1a13b7992
Lizzhan <118141838+Lizzhan@users.noreply.github.com>
2024-10-15 12:08:15 -0700

    demo completed

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,7 +3,15 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
-    console.log($('#footerPlaceholder').load('./text/footer.html'));
+    firebase.auth().onAuthStateChanged(user => {
+        if(user){
+            console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html'));
+        }else{
+            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html'));
+        }
+    })
+
 }
 loadSkeleton();  //invoke the function
